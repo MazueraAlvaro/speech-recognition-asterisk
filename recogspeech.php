7eeb1718094237d6d170be1e5c1eb9a11ad69ea3
Alvaro Mazuera <alvaromazuera16@hotmail.com>
2019-07-05 09:25:26 -0500

    First formaly release

--- recogspeech.php
+++ recogspeech.php
@@ -1,21 +1,59 @@
 #!/usr/bin/php -q
 <?php
+/**
+ * Author: Alvaro Mazuera Urrego
+ * 
+ */
 require_once('phpagi.php');
-$agi = new AGI();
 
+/**
+ * The API Key Google Cloud Platform
+ */
+$api_key = "";
+
+/**
+ * The enconding of the recorded file to recognize
+ * Consult Google Cloud Platform Speecht To Text API Documentation
+ * Default for asterisk recorded files: LINEAR16
+ */
+$encodig = "LINEAR16";
+
+/**
+ * The sample rate of the recorded file to recognize
+ * Default for asterisk recorded files: 8kHz
+ */
+$sample_rate = 8000;
+
+/**
+ * The language code supported by Google Cloud Platform Speech to Text API
+ */
+$lang_code = 'en-US';
+
+/**
+ * Absolute path for the asterisk sounds
+ */
+$ast_sounds = "/var/lib/asterisk/sounds";
+/**
+ * Path with file name without extension for the recorded file to recognize
+ */
+$rel_path = "tmp_file_speech";
+
+/**
+ * The file extension allowed in asterisk for the recorded file, wav and gsm ar commonly used.
+ */
+$file_ext = "wav";
+
+/**
+ * Debug script
+ */
+$debug = false;
+
+$agi = new AGI();
 $agi->answer();
 
-// $agi->exec('festival',"prueba\ de\ audio\ con\ festival");
-// $agi->text2wav("Por favor, diga su nombre.");
-// exit;
-
-
-$nombre = recognize(['ct' => 0.5, 'bool'],"custom/nombre");
-// $edad   = recognize(['num', 'ct' => 0.6],"custom/edad");
-// $sexo   = recognize(['ct' => .3],"custom/sexo", 10);
-// $agi->stream_file("custom/edad");
-// $agi->stream_file("custom/sexo");
-echo "Nombre: $nombre, Edad: $edad, Sexo: $sexo";
+// Get recognized phrase whit a 0.5 confidence threshold and a custom prompt
+$data = recognize(['ct' => 0.5],"custom/name");
+$agi->verbose("RECOGNIZED DATA: $data");
 
 /**
  * $rules:
@@ -29,14 +67,18 @@ echo "Nombre: $nombre, Edad: $edad, Sexo: $sexo";
  *  gtoe=> Greather Than or Equal
  *  in  => Between Enum Vals
  *  null=> Nullable
+ * 
+ * $prompt => File name without extension, located in /var/lib/asterisk/sounds, with relative path.
+ * $num_rep=> Number of repetitions if the phrase recognized is invalid according to the $rules
+ * $timeout=> Max time record file to recognize in seconds.
+ * 
+ * return $phrase
  */
 function recognize($rules = [],$prompt = false, $num_rep = 2, $timeout = 15){
     if($num_rep == 0) return false;
     global $agi;
     if($prompt) $agi->stream_file($prompt);
     list($phrase,$confidence) = recognizeRequest($timeout);
-    // $phrase = "";
-    // $confidence = 0;
     if(!is_array($rules)) return $data;
     $valid = true;
     if(isset($rules['ct']) and is_numeric($rules['ct']))
@@ -58,23 +100,32 @@ function recognize($rules = [],$prompt = false, $num_rep = 2, $timeout = 15){
     else return $phrase;
 }
 
+/**
+ * $timeout=> Max time record file to recognize in seconds.
+ * 
+ * return array():
+ *  [$phrase (String recognized phrase), $confidence (Float confidence value)]
+ */
 function recognizeRequest($timeout = 15){
-    global $agi;
-    $api_key = "";
-    $agi->record_file("tmp/temp_file","wav","#", $timeout*1000, NULL, FALSE, 2);
-    $file = file_get_contents('/var/lib/asterisk/sounds/tmp/temp_file.wav');
+    global $agi, $api_key, $encodig, $sample_rate, $lang_code, $rel_path, $file_ext, $ast_sounds, $debug;
+
+    //Record the speech file
+    $agi->record_file($rel_path,$file_ext,"#", $timeout*1000, NULL, FALSE, 2);
+    //Get the audio content
+    $file = file_get_contents("$ast_sounds/$rel_path.$file_ext");
+    //Prepare the json data to send
     $data = [
         "config" => [
-            "encoding"=>"LINEAR16",
-            "sampleRateHertz"=> 8000,
-            "languageCode"=> "es-CO",
-            "enableWordTimeOffsets"=> false
+            "encoding"              => $encodig,
+            "sampleRateHertz"       => $sample_rate,
+            "languageCode"          => $lang_code,
+            "enableWordTimeOffsets" => false
         ],
         "audio" => [
             "content" => base64_encode($file)
         ]
     ];
-
+    //Init the curl request
     $ch = curl_init("https://speech.googleapis.com/v1/speech:recognize?key=$api_key");
     curl_setopt_array($ch, array(
         CURLOPT_POST => TRUE,
@@ -84,10 +135,11 @@ function recognizeRequest($timeout = 15){
         ),
         CURLOPT_POSTFIELDS => json_encode($data)
     ));
-
     // Send the request
     $response = curl_exec($ch);
+    //Decode the json response
     $responseData = json_decode($response, TRUE);
+    
     $phrase = "";
     $confidence = 0;
     foreach ($responseData['results'] as $result)
@@ -95,8 +147,9 @@ function recognizeRequest($timeout = 15){
             $phrase .= $altern['transcript'];
             $confidence = $altern['confidence'];
         }
-            //floatval($altern['confidence'])*100;//number_format( floatval($altern['confidence'])*100,2,',');
-    $agi->verbose("FRASE ENCONTRADA: $phrase EXACTITUD: $confidence");
+
+    if($debug)
+        $agi->verbose("PHRASE FOUND: $phrase CONFIDENCE: $confidence");
     return [$phrase, $confidence];
 }
 ?>
